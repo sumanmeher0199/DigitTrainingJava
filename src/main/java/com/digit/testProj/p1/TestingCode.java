0042d829bc819debe6a1b7ba0f24f16209d84099
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-18 12:23:49 +0530

    This is my first commit

---
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -0,0 +1,11 @@
+package com.digit.testProj.p1;
+
+public class TestingCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi from Sachin");
+
+	}
+
+}
