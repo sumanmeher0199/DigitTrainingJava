bb3838cd949dcdd76a13ea00608707805c03faab
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-18 12:28:37 +0530

    New commit

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -5,6 +5,9 @@ public class TestingCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi from Sachin");
+		System.out.println("Hi from Sachin");
+		System.out.println("Hi from Sachin");
+		System.out.println("Hi from Sachin");
 
 	}
 
