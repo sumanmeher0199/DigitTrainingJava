03b80362f975df82946ee77c6c418d97fefec36e
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-18 12:31:02 +0530

    This is my new commit

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -4,11 +4,7 @@ public class TestingCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
-		System.out.println("Hi from Sachin");
+		System.out.println("Hi from Suman");
 
 	}
 
