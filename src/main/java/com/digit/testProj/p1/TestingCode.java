4d1fa35ddd6b0f41b31b3c3fb0c72d7646ec50c2
sumanmeher0199 <139847745+sumanmeher0199@users.noreply.github.com>
2023-07-18 12:39:18 +0530

    Update TestingCode.java

--- src/main/java/com/digit/testProj/p1/TestingCode.java
+++ src/main/java/com/digit/testProj/p1/TestingCode.java
@@ -3,7 +3,6 @@ package com.digit.testProj.p1;
 public class TestingCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hi from Suman");
 
 	}
